c796c3b4d8a2fbdb760caf57425c99e0a9b75e8f
Богдан Сухушин <gr441_sovvi@lab-203-9.tomtit-tomsk.ru>
2026-04-02 18:27:15 +0700

    Подключил базу данных, использовал модели

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'rntpxabg_m1',
+    'username' => 'rntpxabg',
+    'password' => 'V8BLrF',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
